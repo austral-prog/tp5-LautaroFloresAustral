8712de9d85bc78653e0559965165893cf47fbd83
Lautaro Flores <lflores3@mail.austral.edu.ar>
2025-09-08 17:08:32 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -1,10 +1,25 @@
 # Replace the "ANSWER HERE" for your answer
 
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
+    x = int(input('Ingrese el primer número: '))
+    y = int(input('Ingrese el segundo número: '))
+    mayor = max(x, y)
+    return mayor
 
 
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    x = int(input('Ingrese el primer número: '))
+    y = int(input('Ingrese el segundo número: '))
+    z = int(input('Ingrese el tercer número: '))
+    mayor = max(x, y, z)
+    return mayor
+
+#   def max_of_two(x, y):
+#   x = int(input('Ingrese el primer numero: '))
+#   y = int(input('Ingrese el segundo numero: '))
+#   if x < y
+#   return y:
+#   elif y < x
+#   return x:
+#   elif x == y 
+#   return 0
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,50 @@
 # Replace the "ANSWER HERE" for your answer
-
+import math
 def roots(a, b, c):
-    return "ANSWER HERE"
+    d = b**2 - 4*a*c
+    if d < 0:
+        return f'( )'
+    elif d == 0:
+        x = (-b/(2*a))
+        return f'({x})'
+    elif d > 0:
+        r1 = (-b + math.sqrt(d))/(2*a)
+        r2 = (-b - math.sqrt(d))/(2*a)
+        return f'({r1}, {r2})'
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a * (x**2) + b * x + c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    resultado = 'f(x) ='
+    if a != 0:
+        resultado += f' {a} * X^2'
+    if b != 0:
+        if a != 0:
+            resultado += f' + {b} * X'
+        else:
+            resultado += f' {b} * X'
+    if c != 0 or (a == 0 and b == 0):
+        if a != 0 or b != 0:
+            resultado += f' + {c}'
+        else:
+            resultado += f' {c}'
+    return resultado
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    resultado = "f'(x) ="
+
+    if a != 0:
+        resultado += f" {2*a} * X"
+        if b != 0:
+            resultado += f" + {b}"
+    else:
+        if b != 0:
+            resultado += f" {b}"
+        else:
+            resultado += " 0"
+    return resultado
